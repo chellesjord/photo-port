72ff28a14a932c0b1c0290f1c28a313aa6b8dca3
chellesjord <m.kayejordan@gmail.com>
2022-03-17 16:27:00 -0500

    add tests for Gallery and PhotoList

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,12 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
-const {
-  categories = [],
-  setCurrentCategory,
-  currentCategory,
-} = props;
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
+
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  };
 
   return (
     <header className="flex-row px-1">
@@ -23,16 +28,25 @@ const {
             </a>
           </li>
           <li className={"mx-2"}>
-            <span>Contact</span>
+            <span onClick={() => handleClick('Contact')}>
+              Contact
+            </span>
           </li>
           {categories.map((category) => (
-              <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} key = {category.name} >
-                <span onClick={() => { setCurrentCategory(category) }}>
-                 {capitalizeFirstLetter(category.name)}
-                </span>
-              </li>
-            ))
-          }
+            <li
+              className={`mx-1 ${currentCategory.name === category.name
+                }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
